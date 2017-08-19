e0b8a3d0629f8d71bcde16b0a5ba329926611eca
Steffen <acbolic@gmail.com>
2017-08-19 19:03:21 +0200

    Added new endpoint to update posts (requires admin access) and an admin can edit all posts

--- controllers/post.go
+++ controllers/post.go
@@ -3,13 +3,15 @@ package controllers
 import (
 	"net/http"
 	"time"
-	"regexp"
 	"strings"
 
 	"github.com/steffen25/golang.zone/models"
 	"github.com/steffen25/golang.zone/services"
 	"github.com/steffen25/golang.zone/repositories"
 	"github.com/steffen25/golang.zone/app"
+	"github.com/gorilla/mux"
+	"strconv"
+	"github.com/go-sql-driver/mysql"
 )
 
 type PostController struct {
@@ -74,8 +76,61 @@ func (pc *PostController) Create(w http.ResponseWriter, r *http.Request) {
 	NewAPIResponse(&APIResponse{Success: true, Message: "Post created"}, w, http.StatusOK)
 }
 
+func (pc *PostController) Update(w http.ResponseWriter, r *http.Request) {
+	uid, err := services.UserIdFromContext(r.Context())
+	if err != nil {
+		NewAPIError(&APIError{false, "Something went wrong", http.StatusInternalServerError}, w)
+		return
+	}
+	vars := mux.Vars(r)
+	postId, err := strconv.Atoi(vars["id"])
+	if err != nil {
+		NewAPIError(&APIError{false, "Invalid request", http.StatusBadRequest}, w)
+		return
+	}
+	post, err := pc.PostRepository.FindById(postId)
+	if err != nil {
+		// user was not found
+		NewAPIError(&APIError{false, "Could not find post", http.StatusNotFound}, w)
+		return
+	}
 
-func generateSlug(title string) string {
-	re := regexp.MustCompile("[^a-z0-9]+")
-	return strings.Trim(re.ReplaceAllString(strings.ToLower(title), "-"), "-")
+	j, err := GetJSON(r.Body)
+	if err != nil {
+		NewAPIError(&APIError{false, "Invalid request", http.StatusBadRequest}, w)
+		return
+	}
+
+	title, err := j.GetString("title")
+	if err != nil {
+		NewAPIError(&APIError{false, "Title is required", http.StatusBadRequest}, w)
+		return
+	}
+
+	body, err := j.GetString("body")
+	if err != nil {
+		NewAPIError(&APIError{false, "Content is required", http.StatusBadRequest}, w)
+		return
+	}
+	if len(strings.Fields(title)) < 2 {
+		NewAPIError(&APIError{false, "Title is too short", http.StatusBadRequest}, w)
+		return
+	}
+
+	if len(strings.Fields(body)) < 5 {
+		NewAPIError(&APIError{false, "Body is too short", http.StatusBadRequest}, w)
+		return
+	}
+	post.UserID = uid
+	post.UpdatedAt = mysql.NullTime{Time: time.Now(), Valid: true}
+	post.Title = title
+	post.Body = body
+	post.Slug = generateSlug(title)
+	err = pc.PostRepository.Update(post)
+	if err != nil {
+		NewAPIError(&APIError{false, "Could not update post", http.StatusBadRequest}, w)
+		return
+	}
+
+	NewAPIResponse(&APIResponse{Success: true, Message: "Post updated"}, w, http.StatusOK)
 }

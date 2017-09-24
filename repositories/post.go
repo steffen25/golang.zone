446aa5d0e3949f945a96a98a0d777d0bb5ce938a
Steffen <acbolic@gmail.com>
2017-09-24 13:46:03 +0200

    Hotfix for created at yyyyMMddHHmmss

--- repositories/post.go
+++ repositories/post.go
@@ -43,7 +43,8 @@ func (pr *postRepository) Create(p *models.Post) error {
 		return err
 	}
 	defer stmt.Close()
-	_, err = stmt.Exec(p.Title, p.Slug, p.Body, p.CreatedAt, p.UserID)
+
+	_, err = stmt.Exec(p.Title, p.Slug, p.Body, p.CreatedAt.Format("20060102150405"), p.UserID)
 	if err != nil {
 		return err
 	}

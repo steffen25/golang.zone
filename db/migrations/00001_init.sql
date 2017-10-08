3c05719ea26971ee215c2538c1dfef3112d9adfa
Juan Santa Cruz <jsantacruz13@gmail.com>
2017-10-08 11:52:53 -0700

    Fixed a couple issues with goose.

--- db/migrations/00001_init.sql
+++ db/migrations/00001_init.sql
@@ -27,5 +27,5 @@ CREATE TABLE IF NOT EXISTS`posts` (
 
 -- +goose Down
 -- SQL in this section is executed when the migration is rolled back.
-DROP TABLE `users`;
 DROP TABLE `posts`;
+DROP TABLE `users`;

baf5590dc80fc2e76cfc24935a53c9437d45e13d
Steffen <acbolic@gmail.com>
2017-10-17 22:11:07 +0200

    Added author when getting posts by a user id

--- models/post.go
+++ models/post.go
@@ -1,42 +1,45 @@
 package models
 
 import (
-	"time"
-	"github.com/go-sql-driver/mysql"
 	"encoding/json"
+	"github.com/go-sql-driver/mysql"
+	"time"
 )
 
 type Post struct {
-	ID     		int    			`json:"id"`
-	Title  		string 			`json:"title"`
-	Slug 		string 			`json:"slug"`
-	Body   		string 			`json:"body"`
-	UserID 		int 			`json:"userId"`
-	CreatedAt 	time.Time		`json:"createdAt"`
-	UpdatedAt 	mysql.NullTime	`json:"updatedAt"`
+	ID        int            `json:"id"`
+	Title     string         `json:"title"`
+	Slug      string         `json:"slug"`
+	Body      string         `json:"body"`
+	UserID    int            `json:"userId"`
+	Author    string         `json:"author,omitempty"`
+	CreatedAt time.Time      `json:"createdAt"`
+	UpdatedAt mysql.NullTime `json:"updatedAt"`
 }
 
 func (p *Post) MarshalJSON() ([]byte, error) {
 	// TODO: Find a better way to set updatedAt to nil
 	if !p.UpdatedAt.Valid {
 		return json.Marshal(struct {
-			ID int 						`json:"id"`
-			Title string 				`json:"title"`
-			Slug string 				`json:"slug"`
-			Body string					`json:"body"`
-			UserID int					`json:"userId"`
-			CreatedAt time.Time			`json:"createdAt"`
-			UpdatedAt *mysql.NullTime	`json:"updatedAt"`
-		}{p.ID, p.Title, p.Slug, p.Body, p.UserID, p.CreatedAt, nil})
+			ID        int             `json:"id"`
+			Title     string          `json:"title"`
+			Slug      string          `json:"slug"`
+			Body      string          `json:"body"`
+			UserID    int             `json:"userId"`
+			Author    string          `json:"author"`
+			CreatedAt time.Time       `json:"createdAt"`
+			UpdatedAt *mysql.NullTime `json:"updatedAt"`
+		}{p.ID, p.Title, p.Slug, p.Body, p.UserID, p.Author, p.CreatedAt, nil})
 	}
 
 	return json.Marshal(struct {
-		ID int 					`json:"id"`
-		Title string 			`json:"title"`
-		Slug string 			`json:"slug"`
-		Body string				`json:"body"`
-		UserID int				`json:"userId"`
-		CreatedAt time.Time		`json:"createdAt"`
-		UpdatedAt time.Time		`json:"updatedAt"`
-	}{p.ID, p.Title, p.Slug, p.Body, p.UserID, p.CreatedAt, p.UpdatedAt.Time})
+		ID        int       `json:"id"`
+		Title     string    `json:"title"`
+		Slug      string    `json:"slug"`
+		Body      string    `json:"body"`
+		UserID    int       `json:"userId"`
+		Author    string    `json:"author"`
+		CreatedAt time.Time `json:"createdAt"`
+		UpdatedAt time.Time `json:"updatedAt"`
+	}{p.ID, p.Title, p.Slug, p.Body, p.UserID, p.Author, p.CreatedAt, p.UpdatedAt.Time})
 }

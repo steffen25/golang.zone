2107fec364dfae6454dcf8f3be53da0000e0a989
Steffen <acbolic@gmail.com>
2017-10-29 23:36:50 +0100

    Travis debug

--- ci.sql
+++ ci.sql
@@ -1,12 +1,12 @@
 CREATE DATABASE IF NOT EXISTS `golang-zone-ci` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 USE `golang-zone-ci`;
-
+SET @@global.innodb_large_prefix = 1;
 CREATE TABLE IF NOT EXISTS `users` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
-  `name` varchar(191) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
-  `email` varchar(191) NOT NULL DEFAULT '',
-  `password` varchar(191) NOT NULL DEFAULT '',
+  `name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email` varchar(255) NOT NULL DEFAULT '',
+  `password` varchar(255) NOT NULL DEFAULT '',
   `created_at` datetime NOT NULL,
   `updated_at` datetime NOT NULL,
   `admin` tinyint(1) unsigned NOT NULL DEFAULT '0',
@@ -16,9 +16,9 @@ CREATE TABLE IF NOT EXISTS `users` (
 
 CREATE TABLE IF NOT EXISTS`posts` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
-  `title` varchar(191) NOT NULL DEFAULT '',
+  `title` varchar(255) NOT NULL DEFAULT '',
   `body` text NOT NULL,
-  `slug` varchar(191) NOT NULL DEFAULT '',
+  `slug` varchar(255) NOT NULL DEFAULT '',
   `created_at` datetime NOT NULL,
   `updated_at` datetime DEFAULT NULL,
   `user_id` int(11) unsigned NOT NULL,

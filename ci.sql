b2cebd1c883a4e8265c61fc1bb3db6ea94af5708
Steffen <acbolic@gmail.com>
2017-10-29 23:47:00 +0100

    travis mysql 5.7

--- ci.sql
+++ ci.sql
@@ -1,4 +1,4 @@
-CREATE DATABASE IF NOT EXISTS `golang-zone-ci` DEFAULT CHARACTER SET utf8 COLLATE utf8mb4_unicode_ci;
+CREATE DATABASE IF NOT EXISTS `golang-zone-ci` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 USE `golang-zone-ci`;
 
